d4cb0edc569ed232c6533dac73e518e8184da67f
PadejskiJ <jelena.padejski@gmail.com>
2022-11-17 11:15:24 +0100

    changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'dkt64f',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here

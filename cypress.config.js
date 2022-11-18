34f11debbe171ec38b62b6f168c60b712c6fc762
PadejskiJ <jelena.padejski@gmail.com>
2022-11-18 11:31:53 +0100

    test1

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,103 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'dkt64f',
+  projectId: "rq9zsx",
+  env: {},
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      animationDistanceThreshold: 5;
+      arch: "x64";
+      baseUrl: "https://testsheepnz.github.io/BasicCalculator.html#main-body";
+      blockHosts: null;
+      browsers: [
+        {
+          name: "chrome",
+          family: "chromium",
+          channel: "stable",
+          displayName: "Chrome",
+          version: "107.0.5304.107",
+          path: "C:Program Files (x86)GoogleChromeApplicationchrome.exe",
+          minSupportedVersion: 64,
+          majorVersion: "107",
+        },
+        {
+          name: "electron",
+          channel: "stable",
+          family: "chromium",
+          displayName: "Electron",
+          version: "106.0.5249.51",
+          path: "",
+          majorVersion: 106,
+        },
+      ];
+      chromeWebSecurity: true;
+      clientCertificates: [];
+      defaultCommandTimeout: 4000;
+      downloadsFolder: "cypress/downloads";
+      defaultCommandTimeout: 30000;
+      reporter: "cypress-mochawesome-reporter";
+      reporterOptions: {
+        reportDir: "cypress/reports";
+        charts: true;
+        reportPageTitle: "My Test Suite";
+        embeddedScreenshots: true;
+        inlineAssets: true;
+      }
+      video: false;
+      screenshotFolder: "screenshots";
+      excludeSpecPattern: "*.hot-update.js";
+      execTimeout: 60000;
+      experimentalFetchPolyfill: false;
+      experimentalInteractiveRunEvents: false;
+      experimentalModifyObstructiveThirdPartyCode: false;
+      experimentalSessionAndOrigin: false;
+      experimentalSingleTabRunMode: false;
+      experimentalSourceRewriting: false;
+      experimentalStudio: false;
+      experimentalWebKitSupport: false;
+      fileServerFolder: "";
+      fixturesFolder: "cypress/fixtures";
+      hosts: null;
+      includeShadowDom: false;
+      isInteractive: true;
+      keystrokeDelay: 0;
+      modifyObstructiveCode: true;
+      nodeVersion: null;
+      numTestsKeptInMemory: 50;
+      pageLoadTimeout: 60000;
+      platform: "win32";
+      port: null;
+      projectId: "rq9zsx";
+      redirectionLimit: 20;
+      reporter: "spec";
+      reporterOptions: null;
+      requestTimeout: 5000;
+      resolvedNodePath: null;
+      resolvedNodeVersion: null;
+      responseTimeout: 30000;
+      retries: {
+        runMode: 0;
+        openMode: 0;
+      }
+      screenshotOnRunFailure: true;
+      screenshotsFolder: "cypress/screenshots";
+      scrollBehavior: "top";
+      slowTestThreshold: 10000;
+      specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}";
+      supportFile: "cypress/support/e2e.{js,jsx,ts,tsx}";
+      supportFolder: false;
+      taskTimeout: 60000;
+      testIsolation: null;
+      trashAssetsBeforeRuns: true;
+      userAgent: null;
+      video: true;
+      videoCompression: 32;
+      videosFolder: "cypress/videos";
+      videoUploadOnPasses: true;
+      viewportHeight: 660;
+      viewportWidth: 1000;
+      waitForAnimations: true;
+      watchForFileChanges: true;
     },
   },
 });
---
+++ cypress/e2e/myTesting/basicCalculatorTestBuild1.spec.cy.ts
@@ -0,0 +1,162 @@
+import { prototypePage } from "./PrototypePage";
+
+beforeEach(() => {
+  cy.visit("https://testsheepnz.github.io/BasicCalculator.html#main-body");
+  prototypePage.chooseBuild(1);
+});
+
+describe("basic calculator test - happy flow", () => {
+  it("verify add operation", () => {
+    prototypePage.add(3, 2), prototypePage.selectIntegersOnly();
+  });
+
+  it("verify substract operation", () => {
+    prototypePage.substract(3, 2);
+  });
+
+  it("verify multiply operation", () => {
+    prototypePage.multiply(3, 2), prototypePage.selectIntegersOnly();
+  });
+
+  it("verify divide operation", () => {
+    prototypePage.divide(3, 2);
+  });
+
+  it("verify concatanate operation", () => {
+    prototypePage.concatanate(3, 2);
+  });
+});
+
+describe("basic calculator test with two negative numbers", () => {
+  it("verify add operation", () => {
+    prototypePage.add(-3, -2), prototypePage.selectIntegersOnly();
+  });
+
+  it("verify substract operation", () => {
+    prototypePage.substract(-3, -2);
+  });
+
+  it("verify multiply operation", () => {
+    prototypePage.multiply(-3, -2), prototypePage.selectIntegersOnly();
+  });
+
+  it("verify divide operation", () => {
+    prototypePage.divide(-3, -2);
+  });
+
+  it("verify concatanate operation", () => {
+    prototypePage.concatanate(-3, -2);
+  });
+});
+
+describe("basic calculator test with two decimal numbers", () => {
+  it("verify add operation", () => {
+    prototypePage.add(2.2, 2.1);
+  });
+
+  it("verify substract operation", () => {
+    prototypePage.substract(2.2, 2.1);
+  });
+
+  it("verify multiply operation", () => {
+    prototypePage.multiply(2.2, 2.1);
+  });
+  it("verify divide operation", () => {
+    prototypePage.divide(2.2, 2.1);
+  });
+
+  it("verify concatanate operation", () => {
+    prototypePage.concatanate(2.2, 2.1);
+  });
+});
+
+describe("basic calculator test with empty number fields", () => {
+  it("verify add operation", () => {
+    prototypePage.addEmptyNumberFields(" ", " ");
+  });
+
+  it("verify substract operation", () => {
+    prototypePage.substract(" ", " ");
+  });
+
+  it("verify multiply operation", () => {
+    prototypePage.multiply(" ", " ");
+  });
+  it("verify divide operation", () => {
+    prototypePage.divideEmptyNumberFields(" ", " ");
+  });
+
+  it("verify concatanate operation", () => {
+    prototypePage.concatanate(" ", " ");
+  });
+});
+
+describe("basic calculator test with a negative and positive number", () => {
+  it("verify add operation", () => {
+    prototypePage.add(-3, 2), prototypePage.selectIntegersOnly();
+  });
+
+  it("verify substract operation", () => {
+    prototypePage.substract(-3, 2);
+  });
+
+  it("verify multiply operation", () => {
+    prototypePage.multiply(-3, 2), prototypePage.selectIntegersOnly();
+  });
+
+  it("verify divide operation", () => {
+    prototypePage.divide(-3, 2);
+  });
+
+  it("verify concatanate operation", () => {
+    prototypePage.concatanate(-3, 2);
+  });
+});
+
+describe("basic calculator test with char instead of number", () => {
+  it("verify add operation", () => {
+    prototypePage.addCharNumberFields("r", "r")
+   
+  });
+
+  it("verify substract operation", () => {
+    prototypePage.substractCharNumberFields("r", "r")
+  });
+
+  it("verify multiply operation", () => {
+    prototypePage.multiplyCharNumberFields("r", "r")
+  });
+
+  it("verify divide operation", () => {
+    prototypePage.divideCharNumberFields("r", "r")
+  });
+
+  it("verify concatanate operation", () => {
+    prototypePage.concatanate("r", "r")
+    
+  });
+});
+
+describe("basic calculator test with two max lenght numbers", () => {
+  it("verify add operation", () => {
+    prototypePage.add(2222222222, 2222222222),
+      prototypePage.selectIntegersOnly();
+  });
+
+  it("verify substract operation", () => {
+    prototypePage.substract(2222222222, 2222222222);
+  });
+
+  it("verify multiply operation", () => {
+    prototypePage.multiply(2222222222, 2222222222),
+      prototypePage.selectIntegersOnly();
+  });
+
+  it("verify divide operation", () => {
+    prototypePage.divide(2222222222, 2222222222);
+  });
+
+  it("verify concatanate operation", () => {
+    prototypePage.concatanate(2222222222, 2222222222);
+  });
+});
